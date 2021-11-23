0052d83eacdbd5633e92334661d78f82550f6079
maximilianjones <maxcharles500@gmail.com>
2021-11-23 09:26:53 -0800

    commit tree e168527b61111fab19957365ccc4e114463eaa46

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr=['Dave', 'Joe', 'Grace', 'Carol']) {
+   const newArr = [];
+   newArr.push(arr[0]);
+   newArr.push(arr[1]);
+   return newArr;
+}
+
+const returnLastTwoDrivers = function(arr=['Dave', 'Joe', 'Grace', 'Carol']) {
+   const lastDrivers = arr.slice(-2);
+   return lastDrivers;
+ }
+ 
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(argument) {
+     return function(fare) {
+         return fare * argument;
+     }
+ }
+
+ function fareDoubler(fare) {
+     return fare * 2;
+
+ }
+
+ function fareTripler(fare) {
+     return fare * 3;
+ }
+
+ function selectDifferentDrivers(arrayOfDrivers=['Dave', 'Joe', 'Grace', 'Carol'], oldFunction=returnFirstTwoDrivers()) {
+     return oldFunction(arrayOfDrivers)
+ }
